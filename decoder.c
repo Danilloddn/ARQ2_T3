9528f948ff8531177ef34048865fdc19b05c598d
AndersonOyama <anderson.oyama@hotmail.com>
2019-06-11 09:19:38 -0300

    Update

---
+++ decoder.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+#DEFINE ZERO 0
+
+char[] translator(char[] instruction, char[] cmd1, char[] cmd2, char[] cmd3){
+	switch (instruction){
+		case "add":
+			break;
+
+		case "addi":
+			break;
+
+		case "and":
+			break;
+
+		case "andi":
+			break;
+
+		case "b":
+			break;
+
+		case "beq":
+			break;
+
+		case "bgez":
+			break;
+
+		case "bgtz":
+			break;
+
+		case "blez":
+			break;
+
+		case "bne":
+			break;
+
+		case "div";
+		break;
+
+		case "j";
+		break;
+	}
+
+
+
+	return bla
+}
+
